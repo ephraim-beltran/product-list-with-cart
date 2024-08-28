102bcee21c40cc64feb4d0669bd106165c837830
Ephraim Beltran <git@beltran.cc>
2024-08-28 14:45:29 -0400

    Start `RouterComponent`

---
+++ src/router/index.jsx
@@ -0,0 +1,9 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+export function RouterComponent(props) {
+  return <>{props.children}</>;
+}
