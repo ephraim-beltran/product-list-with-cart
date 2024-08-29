51285d56e757fd91128ae3a5f826ea037f8af869
ephraim <github@beltran.cc>
2024-08-29 01:10:24 -0400

    feature(data fetch): implement basic data retrieval and passing

--- src/pages/product-list-with-cart/index.jsx
+++ src/pages/product-list-with-cart/index.jsx
@@ -1,3 +1,36 @@
+import styled from "./ProductList.module.css";
+import { ProductGrid } from "./components/ProductGrid";
+import { useState, useEffect } from "react";
+
+// The function below is for fetch simulation only
+import { fetchSimulated } from "../../mock-api";
 export const ProductList = () => {
-  return <h2>Product List</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [productList, setProductList] = useState([]);
+  const getProductList = async (url) => {
+    const res = await fetchSimulated(url);
+    const data = await res.json();
+    return data;
+  };
+  useEffect(() => {
+    (async () => {
+      const url = "https://fake.url.com/v1";
+      const data = await getProductList(url);
+      setProductList(data);
+      setIsLoading(false);
+    })();
+  }, []);
+
+  return (
+    <>
+      <div className={styled.container}>
+        <h2>Desserts</h2>
+        {!isLoading ? (
+          <ProductGrid productList={productList} />
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </>
+  );
 };
