d49812f7b3de45512bb9de374261df491b815732
ephraim <github@beltran.cc>
2024-08-29 00:13:49 -0400

    style: create basic presentational styling

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,7 @@
+import styled from "./Footer.module.css";
 export const Footer = () => {
   return (
-    <footer>
+    <footer className={styled.footer}>
       Challenge by{" "}
       <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
       Coded by <a href="https://github.com/ephraim-beltran">Ephraim Beltran</a>.
