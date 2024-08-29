b979b938883ce926efb6cf9b4598d37f8c70e747
ephraim <github@beltran.cc>
2024-08-28 23:35:28 -0400

    fix(routing): set proper top level component for routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterComponent } from "./router";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterComponent />
   </StrictMode>,
-)
+);
