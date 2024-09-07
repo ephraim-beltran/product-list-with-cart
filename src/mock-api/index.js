23f22b4b5365a2529e7e8527d893c50f7861e9f3
ephraim <github@beltran.cc>
2024-09-07 10:56:19 -0400

    refactor(mock response): include `id` in response data

--- src/mock-api/index.js
+++ src/mock-api/index.js
@@ -2,6 +2,10 @@ import data from "../data/data.json";
 
 export const fetchSimulated = (url, object) => {
   return Promise.resolve({
-    json: () => Promise.resolve(data),
+    ok: object.response != 200 ? false : true,
+    json: () =>
+      Promise.resolve(
+        data.map((item, index) => ({ ...item, id: index + 100 })),
+      ),
   });
 };
