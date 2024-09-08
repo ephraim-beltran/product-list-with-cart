b16084533d980000e695ab862ffb09eb24acbbdc
ephraim <github@beltran.cc>
2024-09-08 00:10:21 -0400

    refactor(mock-api): create a simulated `ok` response

--- src/mock-api/index.js
+++ src/mock-api/index.js
@@ -1,8 +1,8 @@
 import data from "../data/data.json";
 
-export const fetchSimulated = (url, object) => {
+export const fetchSimulated = () => {
   return Promise.resolve({
-    ok: object.response != 200 ? false : true,
+    ok: true,
     json: () =>
       Promise.resolve(
         data.map((item, index) => ({ ...item, id: index + 100 })),
