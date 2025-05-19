321c9ab2284b7a30b271bf895364897c55b21efa
Ephraim Beltran <github@beltran.cc>
2025-05-19 04:39:32 +0000

    fix: removed remaining context imports

--- src/pages/product-list-with-cart/index.jsx
+++ src/pages/product-list-with-cart/index.jsx
@@ -1,17 +1,14 @@
 import styled from "./ProductList.module.css";
-import { CartProvider } from "./context";
 import { ProductGrid } from "./components/ProductGrid";
 import { ShoppingCart } from "./components/ShoppingCart";
 export const ProductList = () => {
   return (
-    <CartProvider>
-      <div className={`${styled.container} ${styled.defaults}`}>
-        <section className={styled.menu}>
-          <h2>Desserts</h2>
-          <ProductGrid />
-        </section>
-        <ShoppingCart />
-      </div>
-    </CartProvider>
+    <div className={`${styled.container} ${styled.defaults}`}>
+      <section className={styled.menu}>
+        <h2>Desserts</h2>
+        <ProductGrid />
+      </section>
+      <ShoppingCart />
+    </div>
   );
 };
